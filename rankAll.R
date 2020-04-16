ce9e25b092b7f881f42e19fff7437bf934af64dd
ckermes <ckermes2@gmail.com>
2020-04-16 15:03:53 -0400

    updated

---
+++ rankAll.R
@@ -0,0 +1,59 @@
+rankall <- function(outcome, num = "best"){
+  ##Read outcome data
+  dataraw <- read.csv('outcome-of-care-measures.csv',
+                      colClasses = "character", 
+                      na.strings = "Not Available", 
+                      stringsAsFactors = FALSE)
+  
+  data <- dataraw[, c(2,7,11,17,23)]
+  
+  names(data) <- c("hospital", "state", "heart attack",
+                   "heart failure", "pneumonia")
+  
+  data[,3] <- as.numeric(data[,3])
+  data[,4] <- as.numeric(data[,4])
+  data[,5] <- as.numeric(data[,5])
+  
+  ##Check that outcome is valid
+  if (outcome == names(data)[3])
+    outtemp <- 3
+  else if(outcome == names(data)[4])
+    outtemp <- 4
+  else if(outcome == names(data)[5])
+    outtemp <- 5
+  else
+    stop("invalid outcome")
+  
+  ##For each state, find the hospital of the given rank
+  
+  ##Clean data of NA values. ranked is new cleaned data
+  cleanout <- data[!is.na(data[, outtemp]),]
+  ranked <- cleanout[order(cleanout[2], cleanout[outtemp]),
+                    cleanout[1],]
+  
+  sorted <- split(ranked, as.factor(ranked$state))
+  hospName <- vector("character", length = length(sorted))
+  
+  if(num == "best"){
+    for(i in 1:length(ranked))
+      hospName[i] <- ranked[[i]][1,1]
+  }
+  else if(num == "worst"){
+    for(i in 1:length(ranked)){
+        index <- nrow(ranked[[i]])
+        hospName[i] <- ranked[[i]][index,1]
+    }
+  }
+  else if(num %% 1 == 0){
+    for(i in 1:length(ranked))
+        hospName[i] <- sort[[i]][num,1]
+  }
+  
+
+   
+  ##Return a data frame with the hospital names and the 
+  ##(abbreviated) state name
+  stateName <- names(ranked)
+  result <- data.frame(hospital = hospNam, state = stateName,
+                       row.names = stateName)
+}
